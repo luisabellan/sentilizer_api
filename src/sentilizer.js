2eb3406e54ed41c59720a79d05911f0cf53218be
Luis <luis.abellan.dev@gmail.com>
2020-08-19 20:44:19 +0200

    added promisify and python_shell, imported vader - save

---
+++ src/sentilizer.js
@@ -0,0 +1,28 @@
+const {promisify} = require('util')
+let python_shell_run = promisify(require('python_shell').run)
+
+
+export function sentilize(sentence) {
+    return new Promise(
+        (resolve, reject) = {
+            let options = {
+                scriptPath = '/home/luis/git/sentilizer_api/src',
+                args: ['-s', '\"' + sentence + '\"']
+
+            }
+            python_shell_run('vader.py', options).then(results => {
+                let sentiment = results[0]
+                let response = {sentiment: 'Neutral'}
+
+                if (sentiment === 'neg'){
+                    response.sentiment = 'Negative'
+                }else if(sentiment === 'pos'){
+                    response.sentiment = 'Positive'
+                }
+                resolve(response)
+            }).catch(error => {
+                reject(error)
+            })
+        }
+    )
+}
